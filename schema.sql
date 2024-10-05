dd92aa75c7c6d28fecf43859c2efd477c0264656
JVilo <vilo.johanna@gmail.com>
2024-10-05 12:38:40 +0300

    remove unused table

--- schema.sql
+++ schema.sql
@@ -25,30 +25,6 @@ CREATE TABLE IF NOT EXISTS messages (
         FOREIGN KEY(ref_key) REFERENCES messages (id)
 );
 
-CREATE TABLE IF NOT EXISTS "RR_tasks" (
-        id SERIAL NOT NULL,
-        "RRm1_sys" INTEGER,
-        "RRm1_dia" INTEGER,
-        "RRm2_sys" INTEGER,
-        "RRm2_dia" INTEGER,
-        "RRmed1_sys" INTEGER,
-        "RRmed1_dia" INTEGER,
-        "RRmed2_sys" INTEGER,
-        "RRmed2_dia" INTEGER,
-        "RRe1_sys" INTEGER,
-        "RRe1_dia" INTEGER,
-        "RRe2_sys" INTEGER,
-        "RRe2_dia" INTEGER,
-        "RRemed1_sys" INTEGER,
-        "RRemed1_dia" INTEGER,
-        "RRemed2_sys" INTEGER,
-        "RRemed2_dia" INTEGER,
-        user_id INTEGER NOT NULL,
-        active INTEGER,
-        PRIMARY KEY (id),
-        FOREIGN KEY(user_id) REFERENCES users (id)
-);
-
 CREATE TABLE IF NOT EXISTS bmi (
         id SERIAL NOT NULL,
         weight INTEGER,
